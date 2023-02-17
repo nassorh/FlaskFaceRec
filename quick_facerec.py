36243f1253752019274c5ccdfc1441fa69aa5c41
nassorh <nassorh.dev@gmail.com>
2023-02-17 19:54:42 +0000

    Clean up code

--- quick_facerec.py
+++ quick_facerec.py
@@ -2,33 +2,32 @@ import cv2
 import numpy as np
 import os
 
+IMAGES_DIR = 'known_faces'
+FACE_CASCADE = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+EXIT_KEY = "q"
+
 # Load the known face images and convert them to grayscale
-known_faces_dir = 'known_faces'
 known_faces = []
 known_names = []
-for filename in os.listdir(known_faces_dir):
-    image_path = os.path.join(known_faces_dir, filename)
+for filename in os.listdir(IMAGES_DIR):
+    image_path = os.path.join(IMAGES_DIR, filename)
+
     known_names.append(filename.split(".")[0])
-    image = cv2.imread(image_path)
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     known_faces.append(image_gray)
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
+    image = cv2.imread(image_path)
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+camera = cv2.VideoCapture(0)
 
-# Load the Haar cascades for face detection
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# Loop over frames from the camera
 while True:
-    # Read a frame from the camera
-    ret, frame = cap.read()
+    ret, frame = camera.read()
 
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the frame
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    faces = FACE_CASCADE.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
     # Iterate over each detected face
     for (i, (x, y, w, h)) in enumerate(faces):
@@ -56,9 +55,8 @@ while True:
     cv2.imshow('Face Recognition', frame)
 
     # Exit on 'q' key press
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord(EXIT_KEY):
         break
 
-# Clean up
 cap.release()
 cv2.destroyAllWindows()
