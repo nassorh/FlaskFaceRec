a0bc873500c6587db91a6ac861a089d92f0f254f
nassorh <nassorh.dev@gmail.com>
2023-03-23 18:03:08 +0000

    A

--- flask_basic.py
+++ flask_basic.py
@@ -3,7 +3,20 @@ import face_recognition
 import cv2
 import numpy as np
 import os
-from face_rec import gen_camera
+from face_rec import fr_thread
+
+def gen_camera():
+    print(fr_thread.IMAGES_DIR)
+    while fr_thread.keep_running:
+        frame = fr_thread.frame
+
+        # Reducing size of streaming
+        frame = cv2.resize(frame, (640, 280))
+        ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+        
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 app = Flask(__name__)
 
