9e1c183a9cb94ef4700dab5dc196a10d6e9507f0
nassorh <nassorh.dev@gmail.com>
2023-03-23 17:59:33 +0000

    a

--- flask_basic.py
+++ flask_basic.py
@@ -3,7 +3,6 @@ import face_recognition
 import cv2
 import numpy as np
 import os
-from camera_test import camera
 from face_rec import fr_thread,gen_camera
 
 app = Flask(__name__)
@@ -11,15 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/ct')
-def camera_test():
-    return render_template('camera.html')
     
-@app.route('/camera_test')
-def plain_video():
-    return Response(camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_camera(), mimetype='multipart/x-mixed-replace; boundary=frame')
