cd63dc61ff50d0d38ecee3c97f9ac1ee338d2445
nassorh <nassorh.dev@gmail.com>
2023-03-23 18:06:55 +0000

    a

--- face_rec.py
+++ face_rec.py
@@ -40,33 +40,34 @@ class FaceRecognitionThread(threading.Thread):
             if not success:
                 break
             else:
-                #Half the size
-                frame_resize = cv2.resize(frame, None, fx=self.SCALE_FACTOR, fy=self.SCALE_FACTOR)
+                #Turn it gray and half the size
+                gray = cv2.cvtColor(cv2.resize(frame, (0, 0), fx=0.25, fy=0.25), cv2.COLOR_BGR2GRAY)
                 current_time = time.time()
                 # Check if enough time has elapsed since the last face recognition
                 if current_time - self.last_recognition_time >= 5:
                     print("Running fac rec..")
-                    # Find all the faces and their encodings in the current frame
-                    face_locations = face_recognition.face_locations(frame_resize)
-                    face_encodings = face_recognition.face_encodings(frame_resize, face_locations)
-
-                    # Iterate over each detected face
-                    for (i, face_encoding) in enumerate(face_encodings):
-                        # Determine if the face is a match for any known face
-
-                        matches = face_recognition.compare_faces(self.known_faces, face_encoding, tolerance=0.6)
-
-                        if len(self.known_faces) == 0:  # Check if there are no known faces
-                            name = "Unknown"
-                        else:
-                            # Determine the best match for the detected face
-                            best_match_idx = np.argmin(face_recognition.face_distance(self.known_faces, face_encoding))
-                            if matches[best_match_idx]:
-                                name = self.known_names[best_match_idx] 
-                            else:
-                                name = "Unknown"
+                    faces = self.faceCascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=7)
+                    for (i, (x, y, w, h)) in enumerate(faces):
+                        print("Found face...")
+                        face = gray[y:y+h, x:x+w]
+                        face_resized = cv2.resize(face, self.known_faces[0].shape[::-1])
                         
-                        print(name)
+                        mse_list = []
+                        for known_face in self.known_faces:
+                            known_face_resized = cv2.resize(known_face, face_resized.shape[::-1])
+                            diff = cv2.absdiff(known_face_resized, face_resized)
+                            mse = np.mean(diff)
+                            mse_list.append(mse)
+                        best_match_idx = np.argmin(mse_list)
+
+                        if mse_list[best_match_idx] < 60:
+                            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                            # cv2.putText(frame, f'{self.known_names[best_match_idx]}', (x, y+h+30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                            print(self.known_names[best_match_idx])
+                        else:
+                            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                            # cv2.putText(frame, 'No match', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                            print("Unknown")
                     self.last_recognition_time = current_time
 
                 # Resize the frame to half of its original size
@@ -83,14 +84,3 @@ class FaceRecognitionThread(threading.Thread):
 
 fr_thread = FaceRecognitionThread()
 fr_thread.start()
-def gen_camera():
-    while fr_thread.keep_running:
-        frame = fr_thread.frame
-
-        # Reducing size of streaming
-        frame = cv2.resize(frame, (640, 280))
-        ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
-        
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
